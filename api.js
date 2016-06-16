2a5f0fdd93303bf62994d7b4119b2770ed7122b3
Joris de Vries <joris@apptrician.nl>
2016-06-16 21:54:03 +0200

    Added 1001 and 1002 commands
    The 1002 command includes a parameter, which has some fixed options.
    Of course these options also are included. :)

--- api.js
+++ api.js
@@ -1,5 +1,18 @@
 /**
- * @api {get} /?custom=1&cmd=3019
+ * @api {get} /?custom=1&cmd=1001 Take a photo
+ * @apiName TakePhoto
+ * @apiGroup Photos
+ */
+
+/**
+ * @api {get} /?custom=1&cmd=1002&par={resolution} Set photo resolution
+ * @apiName SetPhotoResolution
+ * @apiParam {type=12M_4032x3024, 10M_3648x2736, 8M_3264x2448, 5M_2592x1944, 3M_2048x1536, 2MHD_1920x1080, VGA_640x480, 1.3M_1280x960} resolution
+ * @apiGroup Photos
+ */
+
+/**
+ * @api {get} /?custom=1&cmd=3019 Keep alive
  * @apiName KeepAlive
  * @apiGroup System
  */
